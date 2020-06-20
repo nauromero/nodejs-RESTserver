f8a32b90304e00727e14b347d888a21ed6f06761
Nahuel Romero <nromero@Nahuels-MacBook-Pro.local>
2020-06-20 17:07:56 -0300

    variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,8 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB =
-    'mongodb+srv://nau91:OhuCw2m88Xti25kM@cluster0-ugqig.mongodb.net/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
