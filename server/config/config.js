0d4b682e59d80c4cf146a52a3e05128daac75fbb
Nahuel Romero <nromero@Nahuels-MacBook-Pro.local>
2020-06-20 16:43:18 -0300

    REST server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //
 
 process.env.PORT = process.env.PORT || 3000;
+
+//
+// Entorno
+//
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//
+// DB
+//
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB =
+    'mongodb+srv://nau91:OhuCw2m88Xti25kM@cluster0-ugqig.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
