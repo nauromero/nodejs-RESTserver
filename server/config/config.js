9d07a16396e93f8f90668ed5cb38af9f7f235239
Nahuel Romero <nromero@Nahuels-MacBook-Pro.local>
2020-06-22 01:20:42 -0300

    Login normal

--- server/config/config.js
+++ server/config/config.js
@@ -11,15 +11,27 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //
+// Vencimiento del token
+//
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//
+// SEED de autenticación
+//
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+//
 // DB
 //
 
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-  urlDB = 'mongodb://localhost:27017/cafe';
+	urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = process.env.MONGO_URI;
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
